e735b915a74543e4031cbf3752a9690950d0b174
Praveen230389 <Praveenkumar230389@gmail.com>
2025-06-30 04:00:47 +0530

    Update2 app.py

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "<h1>Welcome to Project 2</h1><p>This is served by Flask & Jenkins!</p>"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# app.py
+print("Hello from Jenkins + Python!")
