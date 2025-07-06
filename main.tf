b0680b8f55e6c93f2b63742d5f3c6651fd23f32a
Praveen230389 <Praveenkumar230389@gmail.com>
2025-07-06 19:17:27 +0000

    ADD

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
-  key_name               = "YOUR_KEY_PAIR_NAME"  # Replace with your actual key pair name
+  key_name               = "GlobalSSHkey.pem"  # Replace with your actual key pair name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
