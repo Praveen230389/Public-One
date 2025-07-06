46d010cbecf697ed2ad79cc2ece7ead747786286
Praveen230389 <Praveenkumar230389@gmail.com>
2025-07-06 19:08:12 +0000

    ADD

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
   # your rules
