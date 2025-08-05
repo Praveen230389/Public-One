acf74f64ec44576436ea4af689d74ad17c6968f4
Praveen230389 <Praveenkumar230389@gmail.com>
2025-08-05 13:59:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "praveenkubernetesbucket" {
+resource "aws_s3_bucket" "praveenchaudhary" {
   bucket = "my-unique-terraform-bucket-12345" # bucket name must be globally unique
 }
